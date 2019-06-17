2fa9595d2c3d60469845eb5a53bfd08a573c5982
Alexanderius <alexander.krylkov@gmail.com>
2019-06-17 23:11:23 +0600

    Initial minimal template

---
+++ Minimal/content/Controllers/DefaultController.cs
@@ -0,0 +1,14 @@
+﻿using Simplify.Web;
+using Simplify.Web.Attributes;
+
+namespace Minimal.Controllers
+{
+	[Get("/")]
+	public class DefaultController : Controller
+	{
+		public override ControllerResponse Invoke()
+		{
+			return Ajax("Hello from Simplify.Web minimal application!");
+		}
+	}
+}
---
+++ Minimal/content/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Simplify.Web.Owin;
+
+namespace Minimal
+{
+	public class Startup
+	{
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+
+			app.UseSimplifyWeb();
+		}
+	}
+}
