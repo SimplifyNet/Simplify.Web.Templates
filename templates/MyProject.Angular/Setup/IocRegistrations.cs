72bf3995696d3f166110c7e12c47486e9a13226e
Alexanderius <alexander.krylkov@gmail.com>
2019-06-23 11:41:03 +0600

    Angular application template added

---
+++ templates/MyProject.Angular/Setup/IocRegistrations.cs
@@ -0,0 +1,10 @@
+﻿namespace MyProject.Angular.Setup
+{
+	public static class IocRegistrations
+	{
+		public static void Register()
+		{
+			// Simplify.DI.DIContainer.Current IOC container registrations starting point
+		}
+	}
+}
---
+++ templates/MyProject.Angular/Startup.cs
@@ -0,0 +1,52 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.Extensions.DependencyInjection;
+using MyProject.Angular.Setup;
+using Simplify.DI;
+using Simplify.DI.Provider.Microsoft.Extensions.DependencyInjection;
+using Simplify.Web.Owin;
+
+namespace MyProject.Angular
+{
+	public class Startup
+	{
+		public IServiceProvider ConfigureServices(IServiceCollection services)
+		{
+			var provider = new MicrosoftDependencyInjectionDIProvider { Services = services };
+			DIContainer.Current = provider;
+
+			// Your registrations here (both via services or DIContainer.Current.Register)
+
+			// In production, the Angular files will be served from this directory
+			services.AddSpaStaticFiles(configuration =>
+			{
+				configuration.RootPath = "ClientApp/dist";
+			});
+
+			IocRegistrations.Register();
+
+			return provider.ServiceProvider;
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			if (env.IsDevelopment())
+				app.UseDeveloperExceptionPage();
+
+			app.UseSpaStaticFiles();
+
+			app.UseSimplifyWebNonTerminal();
+
+			app.UseSpa(spa =>
+			{
+				spa.Options.SourcePath = "ClientApp";
+
+				if (env.IsDevelopment())
+					spa.UseAngularCliServer("start");
+			});
+		}
+	}
+}
