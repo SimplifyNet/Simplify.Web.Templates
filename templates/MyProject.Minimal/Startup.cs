f2e0b6ae10ad18ee15de326caad3f3fd212b18d0
Alexanderius <alexander.krylkov@gmail.com>
2022-01-12 23:14:17 +0600

    [#45] [add] minimal project

--- templates/MyProject.Minimal/Startup.cs
+++ templates/MyProject.Minimal/Startup.cs
@@ -1,18 +1,7 @@
-﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
-using Simplify.Web;
+﻿using Simplify.Web;
 
-namespace MyProject.Minimal
-{
-	public class Startup
-	{
-		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-		{
-			if (env.IsDevelopment())
-				app.UseDeveloperExceptionPage();
+var app = WebApplication.CreateBuilder(args).Build();
 
-			app.UseSimplifyWeb();
-		}
-	}
-}
+app.UseSimplifyWeb();
+
+app.Run();
